17aa5e157f143d6de352eaa13db4b393a321f73d
box02 <thebox02@gmail.com>
2009-12-04 20:26:38 +0100

    add a function

--- hello.py
+++ hello.py
@@ -3,7 +3,13 @@
 
 def hello():
     u = raw_input("what planet you would like to live on? ")
-    print "You are now on %s." % u
+    print "You are now on %s.\n" % u
+
+def curiosity():
+    g = raw_input("what is your favorite programming language? ")
+    print "Oh, you like %s, but mine is Python.\n" % g
 
 if __name__ == '__main__':
 	hello()
+    curiosity()
+    print "Have a nice day, Bye!\n"
